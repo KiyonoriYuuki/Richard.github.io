2158e5cd25006ac1cbd75b076e027a2f1503ed1a
reustac1 <reustac1@binghamton.edu>
2024-05-14 18:47:48 -0400

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "Nobody Gets Me",
+        artist: "SZA",
+        img: "Nobody Gets Me",
+        src: "Nobody Gets Me"
+    },
+    {
+        name: "Hold On (with Georgia Ku)",
+        artist: "ILLENIUM & Georgia Ku",
+        img: "music-1",
+        src: "Hold On (with Georgia Ku)"
+    },
+    {
+        name: "Back To December",
+        artist: "Taylor Swift",
+        img: "Back To December",
+        src: "Back To December"
+    },
+    {
+        name: "Takeaway",
+        artist: "Chainsmokers & Illenium ft. Lennon Stella",
+        img: "Takeaway",
+        src: "Takeaway"
+    },
+    {
+        name: "HEAVEN",
+        artist: "The Kid LAROI",
+        img: "Heaven",
+        src: "HEAVEN"
+    },
+    {
+        name: "Someone To Forget - BUSTED by Herobust",
+        artist: "ARMNHMR",
+        img: "Someone To Forget",
+        src: "Someone To Forget - BUSTED by Herobust"
+    },
+    
+    ];
