0956dc1f388d7b86ab42a52da53a94d15759c9e4
reustac1 <reustac1@binghamton.edu>
2024-05-16 13:25:45 -0400

    added password

---
+++ js/password.js
@@ -0,0 +1,9 @@
+var password;
+var pass1="password";
+password=prompt('Please enter your password to view this page!',' ');
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+    }
